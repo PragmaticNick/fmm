69c35fed6f77837c872da144895036cdaf008794
Nikita Skrynnik <nikita.nstu@gmail.com>
2024-11-23 19:14:51 +1100

    add multipole for Y axis

--- fmm-yt/main.cpp
+++ fmm-yt/main.cpp
@@ -2,28 +2,35 @@
 #include <iomanip>
 #include <vector>
 
+#include <glm/glm.hpp>
+
 #include "planet.h"
 #include "fmm.h"
 
 int main()
 {
 	std::vector<planet> planets;
-	generate_planets(10, 0.0, 1.0, planets);
+	generate_planets(10, 1000999, 1100999, planets);
 
 	planet target = {};
-	target.position = { 500.0, 500.0 };
+	target.position = { 50.0, 0.0 };
 	target.mass = 10.0;
 
 	p2p(planets, target);
 
 	std::cout << std::setprecision(15) << "(" << target.force.x << ", " << target.force.y << ")" << std::endl;
 
-	multipole m({ 0.25, 0.25 });
+	glm::dvec2 center = { 0.0, 0.0 };
+	for (auto& planet : planets)
+		center += planet.position;
+
+	center /= planets.size();
+	multipole m(center);
 
 	for (int i = 0; i < 100; i++)
 		m.add(planets[i].position, planets[i].mass);
 
-	auto fx = m.calc(target.position, target.mass);
-	std::cout << "(" << fx << ", " << 0.0 << ")" << std::endl;
+	auto f = m.calc(target.position, target.mass);
+	std::cout << "(" << f.x << ", " << f.y << ")" << std::endl;
 	return 0;
 }
