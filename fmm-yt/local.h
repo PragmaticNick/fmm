24fe92ed7dfdf9e9578ff19eabc99acf7f11d194
Nikita Skrynnik <nikita.nstu@gmail.com>
2025-03-07 11:15:47 +1100

    calc ders for y

--- fmm-yt/local.h
+++ fmm-yt/local.h
@@ -4,8 +4,9 @@
 #include "multipole.h"
 
 struct local {
-	glm::dvec2 center;
-    double values[6] = {};
+    glm::dvec2 center;
+    double lx[6] = {};
+    double ly[6] = {};
 
 	local(glm::dvec2 center) : center(center) {}
 
@@ -60,31 +61,84 @@ struct local {
         derivatives[4][0] = (3 * b2 - 12 * a2b) / d7;
         derivatives[4][1] = 3 * (20 * a3b - 7 * ab2 - 8 * ab3) / d9;
         derivatives[4][2] = (-12 * a4 - 15 * b3 + 6 * a2b *(1 + 12 * b)) / d9;
-        derivatives[4][3] = (-3 * b * (120 * a4 + b3 * (7 + 8 * b) - 4 * a2b * (14 + 31 * b))) / d11;
+        derivatives[4][3] = -3 * b * (120 * a4 + b3 * (7 + 8 * b) - 4 * a2b * (14 + 31 * b)) / d11;
         derivatives[4][4] = (60 * a5 + 3 * ab3 * (49 + 48 * b) - 6 * a3b * (7 + 92 * b)) / d11;
         derivatives[4][5] = (90 * b4 + 6 * a4 * (1 + 42 * b) - 3 * a2b2 * (31 + 168 * b)) / d11;
 
         derivatives[5][0] = (3 * a3 - 12 * ab2) / d7;
-        derivatives[5][1] = (-3 * (4 * a4 - 27 * a2b2 + 4 * b4)) / d9;
-        derivatives[5][2] = (15 * (-3 * a3b + 4 * ab3)) / d9;
+        derivatives[5][1] = -3 * (4 * a4 - 27 * a2b2 + 4 * b4) / d9;
+        derivatives[5][2] = 15 * (-3 * a3b + 4 * ab3) / d9;
         derivatives[5][3] = 15 * (4 * a5 - 41 * a3b2 + 18 * ab4) / d11;
         derivatives[5][4] = 15 * (18 * a4b - 41 * a2b3 + 4 * b5) / d11;
-        derivatives[5][5] = (-45 * (a5 - 12 * a3b2 + 8 * ab4)) / d11;
+        derivatives[5][5] = -45 * (a5 - 12 * a3b2 + 8 * ab4) / d11;
 
         for (int i = 0; i < 6; i++)
             for (int j = 0; j < 6; j++)
-                values[i] += derivatives[i][j] * m.values[j];
+                lx[i] += derivatives[i][j] * m.values[j];
+
+        derivatives[0][0] = -b / d3;
+        derivatives[0][1] = 3 * a * b / d5;
+        derivatives[0][2] = (2 * b2 - a2) / d5;
+        derivatives[0][3] = (3 * b3 - 12 * a2b) / d7;
+        derivatives[0][4] = (3 * a3 - 12 * ab2) / d7;
+        derivatives[0][5] = (9 * a2b - 6 * b3) / d7;
+
+        derivatives[1][0] = -3 * a * b / d5;
+        derivatives[1][1] = (12 * a2b - 3 * b3) / d7;
+        derivatives[1][2] = (12 * ab2 - 3 * a3) / d7;
+        derivatives[1][3] = (45 * ab3 - 60 * a3b) / d9;
+        derivatives[1][4] = (12 * a4 - 81 * a2b2 + 12 * b4) / d9;
+        derivatives[1][5] = (45 * a3b - 60 * ab3) / d9;
+
+        derivatives[2][0] = (a2 - 2 * b2) / d5;
+        derivatives[2][1] = (12 * ab2 - 3 * a3) / d7;
+        derivatives[2][2] = (6 * b3 - 9 * a2b) / d7;
+        derivatives[2][3] = (12 * a4 - 81 * a2b2 + 12 * b4) / d9;
+        derivatives[2][4] = (45 * a3b - 60 * ab3) / d9;
+        derivatives[2][5] = -3 * (3 * a4 - 24 * a2b2 + 8 * b4) / d9;
+
+        derivatives[3][0] = (3 * b3 - 12 * ab2) / d7;
+        derivatives[3][1] = (72 * a2b2 - 21 * ab3 - 12 * b4) / d9;
+        derivatives[3][2] = (-24 * a3b + 3 * a2b2 + 60 * ab3 - 12 * b4) / d9;
+        derivatives[3][3] = -21 * (24 * a3b2 - 8 * a2b3 - 12 * a4 + b5) / d11;
+        derivatives[3][4] = 3 * (48 * a4b - 21 * a3b2 - 184 * a2b3 + 42 *ab4 + 20 * b5) / d11;
+        derivatives[3][5] = 3 * (-8 * a5 + 6 *a4b + 124 * a3b2 - 37 * a2b3 - 120 * ab4 + 20 * b5) / d11;
+
+        derivatives[4][0] = (3 * a3 - 12 * ab2) / d7;
+        derivatives[4][1] = -3 * (4 * a4 - 27 * a2b2 + 4 * b4) / d9;
+        derivatives[4][2] = 15 * (-3 * a3b + 4 * ab3) / d9;
+        derivatives[4][3] = 15 * (4 * a5 - 41 * a3b2 + 18 * ab4) / d11;
+        derivatives[4][4] = 15 * (18 * a4b - 41 * a2b3 + 4 * b5) / d11;
+        derivatives[4][5] = -45 * (a5 - 12 * a3b2 + 8 * ab4) / d11;
+
+        derivatives[5][0] = (9 * a2b - 6 * b3) / d7;
+        derivatives[5][1] = (-45 * a3b + 60 * ab3) / d9;
+        derivatives[5][2] = (9 * a4 - 72 * a2b2 + 24 * b4) / d9;
+        derivatives[5][3] = 15 * (18 * a4b - 41 * a2b3 + 4 * b5) / d11;
+        derivatives[5][4] = -45 * (a5 - 12 * a3b2 + 8 * ab4) / d11;
+        derivatives[5][5] = -15 * (15 * a4b - 40 * a2b3 + 8 * b5) / d11;
+
+        for (int i = 0; i < 6; i++)
+            for (int j = 0; j < 6; j++)
+                ly[i] += derivatives[i][j] * m.values[j];
 	}
 
     glm::dvec2 calc(glm::dvec2 point, double m)
     {
         glm::dvec2 result = { 0.0, 0.0 };
-        result.x += values[0];
-        result.x += values[1] * (point.x - center.x);
-        result.x += values[2] * (point.y - center.y);
-        result.x += values[3] * 0.5 * (point.x - center.x) * (point.x - center.x);
-        result.x += values[4] * (point.x - center.x) * (point.y - center.y);
-        result.x += values[5] * 0.5 * (point.y - center.y) * (point.y - center.y);
+        result.x += lx[0];
+        result.x += lx[1] * (point.x - center.x);
+        result.x += lx[2] * (point.y - center.y);
+        result.x += lx[3] * 0.5 * (point.x - center.x) * (point.x - center.x);
+        result.x += lx[4] * (point.x - center.x) * (point.y - center.y);
+        result.x += lx[5] * 0.5 * (point.y - center.y) * (point.y - center.y);
+
+        result.y += ly[0];
+        result.y += ly[1] * (point.x - center.x);
+        result.y += ly[2] * (point.y - center.y);
+        result.y += ly[3] * 0.5 * (point.x - center.x) * (point.x - center.x);
+        result.y += ly[4] * (point.x - center.x) * (point.y - center.y);
+        result.y += ly[5] * 0.5 * (point.y - center.y) * (point.y - center.y);
 
         return G * m * result;
     }
--- fmm-yt/main.cpp
+++ fmm-yt/main.cpp
@@ -58,18 +58,18 @@ int main()
 	random_planets(100, 0.0, 1.0, planets1);
 	random_planets(100, 1.0, 2.0, planets2);
 	random_planets(100, 2.0, 3.0, planets3);
-	random_planets(100, 10300.0, 10301.0, targets);
+	random_planets(100, 103.0, 104.0, targets);
 
 	auto m1 = new multipole({ 0.5, 0.5 });
 	auto m2 = new multipole({ 1.5, 1.5 });
 	auto m3 = new multipole({ 2.5, 2.5 });
-	auto l = new local({ 10300.5, 10300.5 });
 	for (int i = 0; i < 100; i++) {
 		m1->add(planets1[i].position, planets1[i].mass);
 		m2->add(planets2[i].position, planets2[i].mass);
 		m3->add(planets3[i].position, planets3[i].mass);
 	}
 
+	auto l = new local({ 103.5, 103.5 });
 	l->add(*m1);
 	l->add(*m2);
 	l->add(*m3);
@@ -80,18 +80,14 @@ int main()
 
 	planets1.insert(planets1.end(), planets2.begin(), planets2.end());
 	planets1.insert(planets1.end(), planets3.begin(), planets3.end());
-
 	std::vector<glm::dvec2> p2p_forces(100);
 	for (int i = 0; i < 100; i++)
 	{
 		p2p(planets1, targets[i]);
 		p2p_forces[i] = targets[i].force;
-		p2p_forces[i].y = 0.0;
 	}
 
-	double e = error(p2p_forces, fmm_forces);
-
-	std::cout << "Error: " << e << std::endl;
+	std::cout << "Error: " << error(p2p_forces, fmm_forces) << std::endl;
 
 	return 0;
 }
